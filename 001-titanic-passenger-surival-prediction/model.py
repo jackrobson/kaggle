7f8450c2119ec8c2f7b9feb0d2af2d3e92503ed6
Jack Robson <>
2020-02-29 21:16:53 +0000

    Followed Kaggle introduction tutorial (https://www.kaggle.com/alexisbcook/titanic-tutorial)

---
+++ 001-titanic-passenger-surival-prediction/model.py
@@ -0,0 +1,28 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import os
+for dirname, _, filenames in os.walk('../input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+train_data = pd.read_csv('../input/titanic/train.csv')
+test_data = pd.read_csv('../input/titanic/test.csv')
+
+from sklearn.ensemble import RandomForestClassifier
+
+test_data = pd.read_csv('../input/titanic/test.csv')
+
+y = train_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({"PassengerId": test_data.PassengerId, "Survived": predictions})
+output.to_csv("my_submission.csv", index=False)
+print("Your submission has been successfully saved!")
