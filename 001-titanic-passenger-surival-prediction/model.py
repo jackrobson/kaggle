44b9fbe2f1298f7548bbac7593cf20a198aac7b2
Jack Robson <>
2020-03-01 23:38:41 +0000

    ### 001-titanic ### ### Version 8 ### Score: 0.77990 Features: * males perish * females aged < 1 year old survive * lower class females who spent > $20 on fare perish

--- 001-titanic-passenger-surival-prediction/model.py
+++ 001-titanic-passenger-surival-prediction/model.py
@@ -1,4 +1,4 @@
-# Version 7
+# Version 8
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
@@ -7,26 +7,14 @@ train_data = pd.read_csv('../input/titanic/train.csv')
 test_data = pd.read_csv('../input/titanic/test.csv')
 
 def predict(row):
-    # assumes males perish
+    # males perish
     if row["Sex"] == "male":
         return 0
-    # assumes females aged < 1 year old survive
+    # females aged < 1 year old survive
     if row["Age"] < 1:
         return 1
-    # assumes lower class females aged 1-3 perish
-    if row["Pclass"] == 3 and row["Age"] >= 1 and row["Age"] <= 3:
-        return 0
-    # assumes lower class females aged 4-5 survive
-    if row["Pclass"] == 3 and row["Age"] >= 4 and row["Age"] <= 5:
-        return 1
-    # assumes lower class females aged 6-12 perish
-    if row["Pclass"] == 3 and row["Age"] >= 6 and row["Age"] <= 12:
-        return 0
-    # assumes lower class females aged 13-18 survive
-    if row["Pclass"] == 3 and row["Age"] >= 13 and row["Age"] <= 18:
-        return 1
-    # assumes lower class females aged 19+ perish
-    if row["Pclass"] == 3 and row["Age"] >= 19:
+    # lower class females who spent > $20 on fare perish
+    if row["Pclass"] == 3 and row["Fare"] > 20:
         return 0
     return 1
 
