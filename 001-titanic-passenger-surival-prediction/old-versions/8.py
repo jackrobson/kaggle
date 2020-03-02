296007b8059c1366d32235db93fe48c5c7befb0f
Jack Robson <>
2020-03-02 00:05:04 +0000

    ### 001-titanic ### ### Version 9 ### Score: 0.78468 Features: * upper class males aged <= 18 survive * other males perish * lower class females who spent > $20 on fare perish * other females survive

--- 001-titanic-passenger-surival-prediction/old-versions/8.py
+++ 001-titanic-passenger-surival-prediction/old-versions/8.py
@@ -1,4 +1,4 @@
-# Version 9
+# Version 8
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
@@ -9,17 +9,14 @@ test_data = pd.read_csv('../input/titanic/test.csv')
 def predict(row):
     # males perish
     if row["Sex"] == "male":
-        # upper class males aged <= 18 survive
-        if row["Pclass"] == 1 and row["Age"] <= 18:
-            return 1
-        # other males perish
         return 0
-    else:
-        # lower class females who spent > $20 on fare perish
-        if row["Pclass"] == 3 and row["Fare"] > 20:
-            return 0
-        # other females survive
+    # females aged < 1 year old survive
+    if row["Age"] < 1:
         return 1
+    # lower class females who spent > $20 on fare perish
+    if row["Pclass"] == 3 and row["Fare"] > 20:
+        return 0
+    return 1
 
 predictions = [0] * test_data.shape[0]
 
